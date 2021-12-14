0b316c8258f5f643dce6d57bcde7910737027e32
Nadya Angelia <nadya.angel126@gmail.com>
2021-12-14 23:02:58 +0700

    Setting dulu

---
+++ application/controllers/COlshop.php
@@ -0,0 +1,11 @@
+<?php 
+
+class COlshop extends CI_Controller{
+
+    public function index(){
+        $this->load->model('m_toko');
+        $data['tokokue']= $this->m_toko->get_data();
+    }
+}
+
+?>
